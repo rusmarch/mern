054c1b9b3d934c93fe4c3d7c82388b12956032f7
rusmarch <cheustor@gmail.com>
2023-04-04 20:17:17 +0300

    add routes & controllers

--- backend/server.js
+++ backend/server.js
@@ -9,4 +9,7 @@ app.get('/', (req, res) => {
    res.status(200).json({massage: 'Wellcome to Support Dest API!'});
 })
 
+//Routes 
+app.use('/api/users', require('./routes/userRoutes'));
+
 app.listen(PORT, () => console.log(`SERVER IS WORKING ON PORT: ${PORT}`));
