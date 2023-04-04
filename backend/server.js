95a5ed7d3a64fa352994d67cd521a7c6debd208f
rusmarch <cheustor@gmail.com>
2023-04-04 21:58:56 +0300

    Error and exeption handling

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,12 @@
 const express = require('express'); 
 const dotenv = require('dotenv').config(); 
+const { errorHandler } = require('./middleware/errorMiddleware')
 
 const PORT = process.env.PORT || 5000;
 
 const app = express();
+
+app.use(express.json());
 
 app.get('/', (req, res) => {
    res.status(200).json({massage: 'Wellcome to Support Dest API!'});
@@ -11,5 +14,7 @@ app.get('/', (req, res) => {
 
 //Routes 
 app.use('/api/users', require('./routes/userRoutes'));
+
+app.use(errorHandler);
 
 app.listen(PORT, () => console.log(`SERVER IS WORKING ON PORT: ${PORT}`));
