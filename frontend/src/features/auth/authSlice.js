ed3a0c7af9942d08cd973187a01a7e236d24a705
rusmarch <cheustor@gmail.com>
2023-04-10 13:19:55 +0300

    async thunk 2

--- frontend/src/features/auth/authSlice.js
+++ frontend/src/features/auth/authSlice.js
@@ -9,7 +9,7 @@ const initialState = {
 }
 
 export const register = createAsyncThunk('auth/register', async (user, thunkAPI) => {
-   
+   console.log(user)
 })
 
 export const authSlice = createSlice({
