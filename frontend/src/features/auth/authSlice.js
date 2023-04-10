eb9821cba6deb6dd2ab6e77aa3e4fff84eae6c2d
rusmarch <cheustor@gmail.com>
2023-04-10 11:52:48 +0300

    register bug

--- frontend/src/features/auth/authSlice.js
+++ frontend/src/features/auth/authSlice.js
@@ -1,4 +1,5 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import authService from "./authService";
 
 const initialState = {
    user: null,
@@ -10,29 +11,58 @@ const initialState = {
 
 export const register = createAsyncThunk(
    'auth/register',
-    async (user, thunkAPI) => {
-      console.table(user);
+   async (user, thunkAPI) => {
+      try {
+         return await authService.register(user)
+      } catch (error) {
+         const message = (error.response
+            && error.response.data
+            && error.response.message)
+            || error.message || error.toString();
+
+         return thunkAPI.rejectWithValue(message);
+      }
    })
 
-   export const login = createAsyncThunk(
+export const login = createAsyncThunk(
    'auth/login',
-    async (user, thunkAPI) => {
+   async (user, thunkAPI) => {
       console.table(user);
    })
-
 
 
 export const authSlice = createSlice({
    name: 'auth',
    initialState,
    reducers: {
-
+      reset: (state) => {
+         state.isLoading = false;
+         state.isError = false;
+         state.isSuccess = false;
+         state.message = '';
+      }
    },
    extraReducers: (builder) => {
-
+      builder
+      .addCase(register.pending, (state) => {
+         state.isLoading = true;
+      })
+      .addCase(register.fulfilled, (state, action) => {
+         state.isLoading = false;
+         state.isSuccess = true;
+         state.user = action.payload;
+      }) 
+      .addCase(register.rejected, (state, action) => {
+         state.isLoading = false;
+         state.isError = true;
+         state.message = action.payload;
+         state.user = null;
+      })
    }
 })
 
+
+export const { reset } = authSlice.actions;
 
 // export const selectUser = (state) => state.auth.user;
 // export const selectIsError = (state) => state.auth.isError; 
--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,9 +1,11 @@
-import { useState } from "react"
+import { useState, useEffect } from "react";
+import { useNavigate } from 'react-router-dom';
 import { FaUser } from "react-icons/fa";
 import { toast } from 'react-toastify';
 import { useSelector, useDispatch } from 'react-redux';
 import {
    register,
+   reset
 } from "../features/auth/authSlice";
 
 export const Register = () => {
@@ -20,8 +22,24 @@ export const Register = () => {
    const user = useSelector(state => state.auth.user);
    const isLoading = useSelector(state => state.auth.isLoading);
    const isSuccess = useSelector(state => state.auth.isSuccess);
+   const isError = useSelector(state => state.auth.isError);
    const message = useSelector(state => state.auth.message);
    const dispatch = useDispatch();
+   const navigate = useNavigate();
+
+   useEffect(() => {
+      if (isError) {
+         toast.error(message);
+      }
+      //Redirect when logged in
+      if (isSuccess || user) {
+         navigate('/');
+      }
+
+      dispatch(reset());
+
+   }, [isError, isSuccess, user, message, navigate, dispatch])
+
 
    const onChange = (e) => {
       setFormData((prev) => ({
@@ -48,7 +66,7 @@ export const Register = () => {
    return (
       <>
          <section className="heading">
-            <h1><FaUser />Register {user}</h1>
+            <h1><FaUser />Register</h1>
             <p>Please create an accont</p>
          </section>
          <section className="form">
