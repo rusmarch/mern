51b9232e934ace407b37369a1d3402a441ba433e
rusmarch <cheustor@gmail.com>
2023-04-13 21:11:30 +0300

    get & create ticket - backend

--- backend/controllers/ticketController.js
+++ backend/controllers/ticketController.js
@@ -1,17 +1,48 @@
 const asyncHandler = require('express-async-handler');
 const User = require('../models/userModel');
-const Ticket = require('../models/userModel');
+const Ticket = require('../models/ticketModel');
 
 // Get user ticket | GET /api/tickets | private
 const getTickets = asyncHandler(async (req, res) => {
 
-   res.status(200).json({message: 'getTickets'});
+   // Get user using the id in JWT
+   const user = await User.findById(req.user.id);
+
+   if (!user) {
+      res.status(401)
+      throw new Error('User not found');
+   }
+
+   const tickets = await Ticket.find({ user: req.user.id })
+
+   res.status(200).json(tickets);
 })
 
 // Create new ticket | POST /api/tickets/create | private
 const createTicket = asyncHandler(async (req, res) => {
- 
-   res.status(200).json({message: 'createTicket'});
+
+   const { product, description } = req.body;
+
+   if (!product || !description) {
+      res.status(400);
+      throw new Error('Please add product and description');
+   }
+
+   const user = await User.findById(req.user.id);
+
+   if (!user) {
+      res.status(401);
+      throw new Error('User not found');
+   }
+
+   const ticket = await Ticket.create({
+      product,
+      description,
+      user: req.user.id,
+      status: 'new'
+   })
+
+   res.status(200).json(ticket);
 })
 
 module.exports = {
--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,7 +1,6 @@
 const asyncHandler = require('express-async-handler');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
-
 const User = require('../models/userModel');
 
 const registerUser = asyncHandler(async (req, res) => {
